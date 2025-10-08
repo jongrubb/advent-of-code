69829d8ef3976813e0903e9d9a7c1a1f8f7db4ce
Jon Grubb <grubb2k7@gmail.com>
2025-10-07 23:20:42 -0400

    completed day 5

--- 2024/src/day5.rs
+++ 2024/src/day5.rs
@@ -1,18 +1,20 @@
 use std::{
+    cmp::Ordering,
     collections::{HashMap, HashSet},
-    hash::Hash,
 };
 
 use aoc_runner_derive::aoc;
 
+type PageNumber = u16;
+
 struct PageOrderingRule {
-    page_before: u16,
-    page_after: u16,
+    page_before: PageNumber,
+    page_after: PageNumber,
 }
 
 struct PageOrderingRules {
-    page_numbers_before_map: HashMap<u16, HashSet<u16>>,
-    page_numbers_after_map: HashMap<u16, HashSet<u16>>,
+    page_numbers_before_map: HashMap<PageNumber, HashSet<PageNumber>>,
+    page_numbers_after_map: HashMap<PageNumber, HashSet<PageNumber>>,
 }
 
 impl PageOrderingRules {
@@ -47,13 +49,21 @@ impl PageOrderingRules {
         page_numbers_after.insert(page_ordering_rule.page_after);
     }
 
-    fn is_page_number_before(&self, page_number_before: u16, page_number: u16) -> bool {
+    fn is_page_number_before(
+        &self,
+        page_number_before: PageNumber,
+        page_number: PageNumber,
+    ) -> bool {
         self.page_numbers_before_map
             .get(&page_number)
             .is_some_and(|page_numbers_before| page_numbers_before.contains(&page_number_before))
     }
 
-    fn page_numbers_are_before(&self, page_numbers_before: &Vec<u16>, page_number: u16) -> bool {
+    fn page_numbers_are_before(
+        &self,
+        page_numbers_before: &Vec<PageNumber>,
+        page_number: PageNumber,
+    ) -> bool {
         page_numbers_before
             .iter()
             .find(|page_number_before| {
@@ -62,13 +72,17 @@ impl PageOrderingRules {
             .is_none()
     }
 
-    fn is_page_number_after(&self, page_number_after: u16, page_number: u16) -> bool {
+    fn is_page_number_after(&self, page_number_after: PageNumber, page_number: PageNumber) -> bool {
         self.page_numbers_after_map
             .get(&page_number)
             .is_some_and(|page_numbers_after| page_numbers_after.contains(&page_number_after))
     }
 
-    fn page_numbers_are_after(&self, page_numbers_after: &Vec<u16>, page_number: u16) -> bool {
+    fn page_numbers_are_after(
+        &self,
+        page_numbers_after: &Vec<PageNumber>,
+        page_number: PageNumber,
+    ) -> bool {
         page_numbers_after
             .iter()
             .find(|page_number_after| !self.is_page_number_after(**page_number_after, page_number))
@@ -76,7 +90,7 @@ impl PageOrderingRules {
     }
 }
 
-fn parse(input: &str) -> (Vec<PageOrderingRule>, Vec<Vec<u16>>) {
+fn parse(input: &str) -> (Vec<PageOrderingRule>, Vec<Vec<PageNumber>>) {
     let split_vec: Vec<&str> = input.split("\n\n").collect();
 
     let page_ordering_rules = split_vec.get(0).unwrap();
@@ -84,8 +98,10 @@ fn parse(input: &str) -> (Vec<PageOrderingRule>, Vec<Vec<u16>>) {
     let page_ordering_rules: Vec<PageOrderingRule> = page_ordering_rules
         .lines()
         .map(|l| {
-            let parsed_page_numbers: Vec<u16> =
-                l.split("|").map(|n| n.parse::<u16>().unwrap()).collect();
+            let parsed_page_numbers: Vec<PageNumber> = l
+                .split("|")
+                .map(|n| n.parse::<PageNumber>().unwrap())
+                .collect();
             let page_before = parsed_page_numbers.get(0).unwrap();
             let page_after = parsed_page_numbers.get(1).unwrap();
             PageOrderingRule {
@@ -97,20 +113,24 @@ fn parse(input: &str) -> (Vec<PageOrderingRule>, Vec<Vec<u16>>) {
 
     let page_orderings = split_vec.get(1).unwrap();
 
-    let page_orderings: Vec<Vec<u16>> = page_orderings
+    let page_orderings: Vec<Vec<PageNumber>> = page_orderings
         .lines()
-        .map(|l| l.split(",").map(|n| n.parse::<u16>().unwrap()).collect())
+        .map(|l| {
+            l.split(",")
+                .map(|n| n.parse::<PageNumber>().unwrap())
+                .collect()
+        })
         .collect();
 
     return (page_ordering_rules, page_orderings);
 }
 
-fn get_middle(numbers: &Vec<u16>) -> u16 {
+fn get_middle(numbers: &Vec<PageNumber>) -> PageNumber {
     *numbers.get((numbers.len() - 1) / 2).unwrap()
 }
 
 #[aoc(day5, part1)]
-fn part1(input: &str) -> u16 {
+fn part1(input: &str) -> PageNumber {
     let (rules, page_orderings) = parse(input);
 
     let page_ordering_rules = PageOrderingRules::new_and_populate(&rules);
@@ -118,8 +138,8 @@ fn part1(input: &str) -> u16 {
     page_orderings
         .iter()
         .filter_map(|page_ordering| {
-            let mut page_numbers_before: Vec<u16> = Vec::new();
-            let mut page_numbers_after: Vec<u16> = page_ordering.iter().copied().collect();
+            let mut page_numbers_before: Vec<PageNumber> = Vec::new();
+            let mut page_numbers_after: Vec<PageNumber> = page_ordering.iter().copied().collect();
 
             let mut are_page_numbers_in_proper_order = true;
 
@@ -142,21 +162,51 @@ fn part1(input: &str) -> u16 {
 }
 
 #[aoc(day5, part2)]
-fn part2(input: &str) -> String {
-    todo!()
+fn part2(input: &str) -> PageNumber {
+    let (rules, page_orderings) = parse(input);
+
+    let page_ordering_rules = PageOrderingRules::new_and_populate(&rules);
+
+    let mut sum = 0;
+
+    for mut page_ordering in page_orderings {
+        let page_numbers_in_proper_order = {
+            let mut page_numbers_before: Vec<PageNumber> = Vec::new();
+            let mut page_numbers_after: Vec<PageNumber> = page_ordering.iter().copied().collect();
+
+            let mut are_page_numbers_in_proper_order = true;
+
+            while !page_numbers_after.is_empty() {
+                let page_number = page_numbers_after.remove(0);
+
+                if !page_ordering_rules.page_numbers_are_before(&page_numbers_before, page_number)
+                    || !page_ordering_rules.page_numbers_are_after(&page_numbers_after, page_number)
+                {
+                    are_page_numbers_in_proper_order = false;
+                    break;
+                }
+
+                page_numbers_before.push(page_number);
+            }
+
+            are_page_numbers_in_proper_order
+        };
+
+        if !page_numbers_in_proper_order {
+            page_ordering.sort_by(|a: &u16, b| {
+                page_ordering_rules
+                    .is_page_number_before(*a, *b)
+                    .then(|| Ordering::Less)
+                    .or_else(|| Some(Ordering::Greater))
+                    .unwrap()
+            });
+
+            sum = sum + get_middle(&page_ordering)
+        }
+    }
+
+    sum
 }
 
 #[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn part1_example() {
-        assert_eq!(part1(&parse("<EXAMPLE>")), "<RESULT>");
-    }
-
-    #[test]
-    fn part2_example() {
-        assert_eq!(part2(&parse("<EXAMPLE>")), "<RESULT>");
-    }
-}
+mod tests {}
